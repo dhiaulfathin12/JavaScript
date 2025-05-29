a370dc556de079c0a121cbb1b99d44b2d15990c9
DhiaulFathin <fathin@wahin.com>
2025-05-29 10:00:16 +0700

    new

--- app.js
+++ app.js
@@ -5,9 +5,9 @@
 // //pop up tp ada textboxnya
 // prompt("Insert Your Name", "Your Name")
 
-//variable deklarasi 
-//var, let, const
-//var jarang digunakan, lebih baik gunakan let atau const
+// variable deklarasi 
+// var, let, const
+// var jarang digunakan, lebih baik gunakan let atau const
 
 // let nama = "Syahansyah Fathin";
 // const tnglahir = "20 Mei 2007";
@@ -20,13 +20,14 @@
 
 
 
-//tipe data primitif
-//string, number, boolean, null undifiend, symbol
 
-//string
-let nama = "Asep";
-let alamat = 'Jakarta';
-let hello = `Hallo, selamat datang ${nama} kamu sekarang sedang di ${alamat}`;
+// //tipe data primitif
+// //string, number, boolean, null undifiend, symbol
+
+// //string
+// let nama = "Asep";
+// let alamat = 'Jakarta';
+// let hello = `Hallo, selamat datang ${nama} kamu sekarang sedang di ${alamat}`;
 
 // console.log('Hallo, selamat datang', nama, 'Kamu sekarang sedang di', alamat)
 
@@ -34,69 +35,102 @@ let hello = `Hallo, selamat datang ${nama} kamu sekarang sedang di ${alamat}`;
 // alert("hello")
 
 
-//number
+// //number
 // let umur = 18;
 // let berat = 55.4;
 // let suhu = -3;
 // let sisa = 10%3;
 // let nan = "saya" //karna saya tdk pake kurung jadi dibaca number(NaN)
 
-// console.log(typeof umur)
-// console.log(typeof berat)
-// console.log(typeof suhu)
-// console.log(typeof sisa)
+// console.log(umur)
+// console.log(berat)
+// console.log(suhu)
+// console.log(sisa)
 // console.log(typeof nan)
 
 
-//boolean
+// //boolean
 
-let benerGak = 1 > 3;
+// let benerGak = 1 > 3;
 
-// console.log(typeof benerGak);
-
-
-// null, undifiend
-//null adalah nilai yg sengaja diberikan, artinya tdk ada nilai yg diberikan
-let Adit; //undifiend
+// console.log(benerGak);
 
 
-//tipe data objek
-//objek adalah tipe data kompleks, yg berisi kumpulan nilai dalam bentuk pasangan key-value
+// // null, undifiend
+// //null adalah nilai yg sengaja diberikan, artinya tdk ada nilai yg diberikan
+// let Adit; //undifiend
+// console.log(Adit)
 
-let saya = {
-    nama : 'Syahansyah',
-    umur : '18',
-    alamat : 'jakarta',
-    adik : ['asep', 'budi', 'putri'],
-    rutinitas : {
-        pagi : 'workout',
-        siang : 'masak',
-        malam : 'tidur',
-    },
+// //tipe data objek
+// //objek adalah tipe data kompleks, yg berisi kumpulan nilai dalam bentuk pasangan key-value
 
-    hai: function(){
-        console.log(`hello, nama saya ${this.nama}`)
-    }
-}
+// let saya = {
+//     nama : 'Syahansyah',
+//     tahunlahir : 2007,
+//     umur : `2025 - `,
+//     alamat : 'jakarta',
+//     adik : ['asep', 'budi', 'putri'],
+//     rutinitas : {
+//         pagi : 'workout',
+//         siang : 'masak',
+//         malam : 'tidur',
+//     },
 
-saya.hai()
+//     hai: function(){
+//         console.log(`hello, nama saya ${this.nama}`)
+//     }
 
-console.log(saya);
-console.log(saya.nama);
-console.log(saya.rutinitas.siang);
-console.log('halo saya punya adik 3, yg ketiga', saya.adik[2])
+//     // umur: function(){
+//     //     2025 - ${this.tahunlahir}
+//     // },
+// }
+
+// console.log(saya.umur);
+
+
+// saya.hai()
+
+// console.log(saya);
+// console.log(saya.nama);
+// console.log(saya.rutinitas.siang);
+// console.log('halo saya punya adik 3, yg ketiga', saya.adik[2])
 
 
 
-let buku = {
-    judul : 'Jatuh',
-    author : 'Tere Liye',
-    halaman : 100,
-    genre : ['fantasy', 'drama', 'fiction', 'mystery'],
-    terbit : {
-        tahun : 2010,
-        penerbit : "gramedia",
-        isbn : 299123
-    }
-    
-}
+// let buku = {
+//     judul : 'Jatuh',
+//     author : 'Tere Liye',
+//     halaman : 100,
+//     genre : ['fantasy', 'drama', 'fiction', 'mystery'],
+//     terbit : {
+//         tahun : 2010,
+//         penerbit : "gramedia",
+//         isbn : 299123
+//     }
+
+// }
+
+// //array pada javascript
+
+// let mobil = ['toyota avanza', 'honda jazz', 'suzuki ertiga', 'daihatsu xenia'];
+
+// console.log(`saya punya ${mobil.length} mobil, yaitu, ${mobil[0]}, ${mobil[1]}, ${mobil[2]}, dan ${mobil[3]}`)
+
+
+
+//operator aritmatika
+// let a = 15;
+// let b = 10;
+// let hasil = a + b;
+
+// console.log(`hasil dari ${a} + ${b} = ${hasil}`)
+// console.log(`hasil dari ${a} / ${b} = ${hasil}`)
+// console.log(`hasil dari ${a} * ${b} = ${hasil}`)
+// console.log(`hasil dari ${a} % ${b} = ${hasil}`);
+
+
+let a = prompt("Masukkan alas")
+let b = prompt("Masukkan tinggi")
+let luas = (a * b) / 2;
+
+alert(`Hasil hasil luasnya ${luas}`)
